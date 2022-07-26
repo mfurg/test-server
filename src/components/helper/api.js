ee6d46339eec747d52bd5ba4145bdac91d8948fd
mfurg <mfurgalo@gmail.com>
2022-07-26 17:55:47 +0300

    bad url

--- src/components/helper/api.js
+++ src/components/helper/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001/',
-  //baseURL: 'https://gentle-plateau-38671.herokuapp.com/',
+  //baseURL: 'http://localhost:3001/',
+  baseURL: 'https://gentle-plateau-38671.herokuapp.com/',
 });
 
 instance.interceptors.request.use((c) => {
